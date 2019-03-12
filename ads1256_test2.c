5aaa72e4ff228c5ca3322ace003de9c7887ca65c
n-trenear <44279370+n-trenear@users.noreply.github.com>
2019-03-12 16:49:45 +1000

    Add files via upload

--- ads1256_test2.c
+++ ads1256_test2.c
@@ -881,7 +881,7 @@ int  main()
 			time_t t = time(NULL);
 			struct tm tm = *localtime(&t);
 			fp = fopen ("VoltageReadings.csv", "a+");
-   		fprintf(fp, "%d-%d-%d %d:%d:%d,%ld.%03ld\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour - 2, tm.tm_min + 1, tm.tm_sec, Vin / 1000000, (Vin%1000000)/1000);
+   		fprintf(fp, "%d-%d-%d %d:%d:%d,%ld.%03ld\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour - 2, tm.tm_min, tm.tm_sec, Vin / 1000000, (Vin%1000000)/1000);
 			fclose(fp);
 
 			if (Vin < 0){
