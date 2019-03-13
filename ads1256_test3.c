4822972ef63dabadf152bfeeacf2f2f3942721a5
n-trenear <44279370+n-trenear@users.noreply.github.com>
2019-03-13 14:08:03 +1000

    Add files via upload

--- ads1256_test3.c
+++ ads1256_test3.c
@@ -877,7 +877,7 @@ int  main()
 			}
 
 			Vmeasured = (volt[7] - volt[6]); //Volts
-			printf("%ld.%03ld %03ld V \r\n", Vmeasured / 1000000, (Vmeasured%1000000)/1000, Vmeasured%1000);
+			// printf("%ld.%03ld %03ld V \r\n", Vmeasured / 1000000, (Vmeasured%1000000)/1000, Vmeasured%1000);
 			Rtemp = (5 * 1000 - Vmeasured * (1000 + 1000000)) / 1000;
 			printf("%ld.%03ld %03ld R \r\n", Rtemp / 1000000, (Rtemp%1000000)/1000, Rtemp%1000);
 
